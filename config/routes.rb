c6bb66edff1ed74d6985cf6a8e59fcfb1cc78dfa
Karinna Iniguez <karinnainiguez@gmail.com>
2018-04-02 17:38:26 -0700

    Add blank methods to controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'trips#index'
 
   get '/trips', to: 'trips#index', as: 'trips'
-  get '/trips/new', to: 'trips#new', as 'new_trip'
+  get '/trips/new', to: 'trips#new', as: 'new_trip'
   post '/trips', to: 'trips#create'
 
   get '/trips/:id', to: 'trips#show', as: 'trip'
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   delete '/trips/:id', to: 'trips#destroy'
 
   get '/drivers', to: 'drivers#index', as: 'drivers'
-  get '/drivers/new', to: 'drivers#new', as 'new_driver'
+  get '/drivers/new', to: 'drivers#new', as: 'new_driver'
   post '/drivers', to: 'drivers#create'
 
   get '/drivers/:id', to: 'drivers#show', as: 'driver'
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   delete '/drivers/:id', to: 'drivers#destroy'
 
   get '/passengers', to: 'passengers#index', as: 'passengers'
-  get '/passengers/new', to: 'passengers#new', as 'new_passenger'
+  get '/passengers/new', to: 'passengers#new', as: 'new_passenger'
   post '/passengers', to: 'passengers#create'
 
   get '/passengers/:id', to: 'passengers#show', as: 'passenger'
