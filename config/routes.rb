8a6177248cce215ded64ea575d4d7d5781ff3362
Anne <annehwatson@gmail.com>
2018-04-02 17:31:51 -0700

    updated routes file with routes for each controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,32 @@
 Rails.application.routes.draw do
-  get 'trips/index'
+  root 'trips#index'
 
-  get 'drivers/index'
+  get '/trips', to: 'trips#index', as: 'trips'
+  get '/trips/new', to: 'trips#new', as 'new_trip'
+  post '/trips', to: 'trips#create'
 
-  get 'passengers/index'
+  get '/trips/:id', to: 'trips#show', as: 'trip'
+  get '/trips/:id/edit', to: 'trips#edit', as: 'edit_trip'
+  patch '/trips/:id', to: 'trips#update'
+  delete '/trips/:id', to: 'trips#destroy'
+
+  get '/drivers', to: 'drivers#index', as: 'drivers'
+  get '/drivers/new', to: 'drivers#new', as 'new_driver'
+  post '/drivers', to: 'drivers#create'
+
+  get '/drivers/:id', to: 'drivers#show', as: 'driver'
+  get '/drivers/:id/edit', to: 'drivers#edit', as: 'edit_driver'
+  patch '/drivers/:id', to: 'drivers#update'
+  delete '/drivers/:id', to: 'drivers#destroy'
+
+  get '/passengers', to: 'passengers#index', as: 'passengers'
+  get '/passengers/new', to: 'passengers#new', as 'new_passenger'
+  post '/passengers', to: 'passengers#create'
+
+  get '/passengers/:id', to: 'passengers#show', as: 'passenger'
+  get '/passengers/:id/edit', to: 'passengers#edit', as: 'edit_passenger'
+  patch '/passengers/:id', to: 'passengers#update'
+  delete '/passengers/:id', to: 'passengers#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
